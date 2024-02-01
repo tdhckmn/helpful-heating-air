4bb571539ed56d26b8624c2239a8f1716dd016b5
Tom Hickman <tomh25@protonmail.com>
2024-02-01 18:47:58 -0500

    idk

---
+++ .next/server/pages/_document.js
@@ -0,0 +1,123 @@
+"use strict";
+(() => {
+var exports = {};
+exports.id = 660;
+exports.ids = [660];
+exports.modules = {
+
+/***/ 316:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "default": () => (/* binding */ _document)
+});
+
+// EXTERNAL MODULE: external "react/jsx-runtime"
+var jsx_runtime_ = __webpack_require__(997);
+// EXTERNAL MODULE: ./node_modules/next/document.js
+var next_document = __webpack_require__(859);
+;// CONCATENATED MODULE: ./src/utils/AppConfig.ts
+const AppConfig = {
+    site_name: "React landing page",
+    title: "React landing page template 2021",
+    description: "Production ready plug n play landing page!",
+    locale: "en"
+};
+
+;// CONCATENATED MODULE: ./src/pages/_document.tsx
+
+
+
+// Need to create a custom _document because i18n support is not compatible with `next export`.
+class MyDocument extends next_document["default"] {
+    render() {
+        return /*#__PURE__*/ (0,jsx_runtime_.jsxs)(next_document.Html, {
+            lang: AppConfig.locale,
+            children: [
+                /*#__PURE__*/ jsx_runtime_.jsx(next_document.Head, {}),
+                /*#__PURE__*/ (0,jsx_runtime_.jsxs)("body", {
+                    children: [
+                        /*#__PURE__*/ jsx_runtime_.jsx(next_document.Main, {}),
+                        /*#__PURE__*/ jsx_runtime_.jsx(next_document.NextScript, {})
+                    ]
+                })
+            ]
+        });
+    }
+}
+/* harmony default export */ const _document = (MyDocument);
+
+
+/***/ }),
+
+/***/ 140:
+/***/ ((module) => {
+
+module.exports = require("next/dist/server/get-page-files.js");
+
+/***/ }),
+
+/***/ 716:
+/***/ ((module) => {
+
+module.exports = require("next/dist/server/htmlescape.js");
+
+/***/ }),
+
+/***/ 368:
+/***/ ((module) => {
+
+module.exports = require("next/dist/server/utils.js");
+
+/***/ }),
+
+/***/ 724:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/constants.js");
+
+/***/ }),
+
+/***/ 743:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/html-context.js");
+
+/***/ }),
+
+/***/ 524:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/is-plain-object.js");
+
+/***/ }),
+
+/***/ 689:
+/***/ ((module) => {
+
+module.exports = require("react");
+
+/***/ }),
+
+/***/ 997:
+/***/ ((module) => {
+
+module.exports = require("react/jsx-runtime");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = __webpack_require__.X(0, [859], () => (__webpack_exec__(316)));
+module.exports = __webpack_exports__;
+
+})();
